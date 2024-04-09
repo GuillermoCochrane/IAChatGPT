ac12d02917fb926ac6ce9ce1640e7f5c16032201
GuillermoCochrane <guilleac81@gmail.com>
2024-04-09 05:14:45 -0300

    Agregado tema 2: historia de la IA

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Ideas' });
+  res.render('index', { title: 'Ideas', id: "1" });
 });
 
 router.get('/:id', function(req, res, next) {
-  res.render(req.params.id);
+  res.render(req.params.id,{id: req.params.id});
 });
 
 module.exports = router;
