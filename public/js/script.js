4124b1641a00b7c240bfdc32cd8b96a39348937f
GuillermoCochrane <guilleac81@gmail.com>
2024-04-11 18:37:57 -0300

    Modificados estilos

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,8 @@
 window.addEventListener("load", ()=>{
     const $index = document.querySelector("#index");
     const $sumario = document.querySelector(".sumario");
-    const $body = document.querySelector("body");
-    const $article = document.querySelector("article")
 
     $index.addEventListener("click", function() {
         $sumario.classList.toggle("hidden");
-        $body.classList.toggle("hide-index")
-        if(window.matchMedia("(max-width: 600px)").matches){
-            $article.classList.toggle("hidden");
-            $sumario.classList.toggle("show")
-        }
     })
 })
